1400c12bef20041d7fe3318a6b926d6794c08870
Alearie <beaxcabal@gmail.com>
2023-07-19 14:24:46 +0800

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <img
+          src="/src/assets/images/Details/logo.png"
+          className="logo "
+          alt="logo"
+        />
+        <ul>
+          <li>
+            <a href="#Home"> Home </a>
+          </li>
+          <li>
+            <a href="#Menu"> Menu </a>
+          </li>
+          <li>
+            <a href="#About"> About </a>
+          </li>
+          <li>
+            <a href="#Contact"> Contact </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default App;
