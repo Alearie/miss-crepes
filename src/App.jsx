28fb60ace7cd0d0c362c626a2c40f17f2c4dfc8a
Alearie <beaxcabal@gmail.com>
2023-07-19 14:34:08 +0800

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,36 @@ function App() {
           </li>
         </ul>
       </nav>
+
+      <div id="Home">
+        <div className="container-home">
+          <div className="home-text">
+            <h1> We create delicious memories </h1>
+            <p>
+              Offering a variety of crepe products passionately made from <br />
+              the finest ingredients
+            </p>
+            <a href="#Menu"> Explore more </a>
+          </div>
+          <div className="home-crepes">
+            <img
+              src="src/assets/images/C1.png"
+              className="crepe-1"
+              alt="Crepe1"
+            />
+            <img
+              src="src/assets/images/C4.png"
+              className="crepe-2"
+              alt="Crepe4"
+            />
+            <img
+              src="src/assets/images/C3.png"
+              className="crepe-3"
+              alt="Crepe3"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
